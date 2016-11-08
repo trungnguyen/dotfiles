e72bb7abf76533b78dab2b58769391a95e481c3d
James McKay <code@jamesmckay.cc>
2016-11-08 15:41:06 +0000

    Fix location of .vim bundles and replacement of symlinks

--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# ====== Symlink all dotfiles and dotfolders into root directory ====== #
+
 for I in $(ls -A | grep -v -e "^\.git\(ignore\|attributes\)\?$" | grep ^\\.)
 do
-    if [ -f ~/$I ]; then
+    if [ -f ~/$I ] || [ -h ~/$I ]; then
         rm ~/$I
     fi
     ln -s $DIR/$I ~/$I
 done
 
+# ====== Configure vim ====== #
+
 mkdir -p $DIR/.vim/bundle
-git clone https://github.com/gmarik/vundle $DIR/vim/bundle/vundle
+git clone https://github.com/gmarik/vundle $DIR/.vim/bundle/vundle
 vim -C +PluginInstall +qa!
