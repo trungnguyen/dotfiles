20181f99aefc2d08cb319f23859869d95ba79c7f
James McKay <code@jamesmckay.net>
2016-11-29 15:51:23 +0000

    Move files to by symlinked into "link"

--- install.sh
+++ install.sh
@@ -4,23 +4,23 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # ====== Symlink all dotfiles and dotfolders into root directory ====== #
 
-for I in $(ls -A | grep -v -e "^\.git\(ignore\|attributes\)\?$" | grep ^\\.)
+for I in $(ls -A $DIR/link)
 do
     if [ -f ~/$I ] || [ -h ~/$I ]; then
         rm ~/$I
     fi
-    ln -s $DIR/$I ~/$I
+    ln -s $DIR/link/$I ~/$I
 done
-
-# ====== Configure vim ====== #
-
-if ! [ -d $DIR/.vim/bundle ]; then
-    mkdir -p $DIR/.vim/bundle
-    git clone https://github.com/gmarik/vundle $DIR/.vim/bundle/vundle
-    vim -C +PluginInstall +qa!
-fi
 
 # ====== Append to path ====== #
 
 mkdir -p ~/.bin
-ln -s $DIR/bin ~/.bin/dotfiles
+ln -fhs $DIR/bin ~/.bin/dotfiles
+
+# ====== Configure vim ====== #
+
+if ! [ -d $DIR/link/.vim/bundle ]; then
+    mkdir -p $DIR/link/.vim/bundle
+    git clone https://github.com/gmarik/vundle $DIR/link/.vim/bundle/vundle
+    vim -C +PluginInstall +qa!
+fi
