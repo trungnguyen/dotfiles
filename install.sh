3edd2596b407742ee87a429bae68edfcddcd8422
James McKay <code@jamesmckay.net>
2016-11-29 19:42:26 +0000

    Move "link" folder to "home"

--- install.sh
+++ install.sh
@@ -2,23 +2,26 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if ! [ -f ~/.dotfiles-backup ]; then
-    mkdir -p ~/.dotfiles-backup
+if ! [ -f ~/.dotfiles-backup/home ]; then
+    mkdir -p ~/.dotfiles-backup/home
+fi
+if ! [ -f ~/.dotfiles-backup/local ]; then
+    mkdir -p ~/.dotfiles-backup/local
 fi
 
 # ====== Symlink all dotfiles and dotfolders into root directory ====== #
 
-for I in $(ls -A $DIR/link)
+for I in $(ls -A $DIR/home)
 do
     if [ -f ~/$I ] || [ -h ~/$I ]; then
-        if ! [ -e ~/.dotfiles-backup/$I ]; then
-            echo Backing up $I to ./dotfiles-backup
-            mv ~/$I ~/.dotfiles-backup
+        if ! [ -e ~/.dotfiles-backup/home/$I ]; then
+            echo Backing up $I to .dotfiles-backup/home
+            mv ~/$I ~/.dotfiles-backup/home
         else
             rm ~/$I
         fi
     fi
-    ln -s $DIR/link/$I ~/$I
+    ln -s $DIR/home/$I ~/$I
 done
 
 # ====== Append to path ====== #
@@ -28,8 +31,8 @@ ln -fhs $DIR/bin ~/.bin/dotfiles
 
 # ====== Configure vim ====== #
 
-if ! [ -d $DIR/link/.vim/bundle ]; then
-    mkdir -p $DIR/link/.vim/bundle
-    git clone https://github.com/gmarik/vundle $DIR/link/.vim/bundle/vundle
+if ! [ -d ~/.vim/bundle ]; then
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
     vim -C +PluginInstall +qa!
 fi
