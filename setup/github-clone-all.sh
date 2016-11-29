09418c54c7863b1ad1f367e8f89f4a819efbedbb
James McKay <code@jamesmckay.net>
2016-11-29 18:23:59 +0000

    Script to clone all my public github repos

---
+++ setup/github-clone-all.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+mkdir -p ~/abc/github
+
+for I in $(curl -s https://api.github.com/users/jammycakes/repos | jq -r .[].name)
+do
+    if [ -e ~/abc/github/$I ]; then
+        echo Already cloned $I
+    else
+        git clone git@github.com:jammycakes/$I ~/abc/github/$I
+    fi
+done
